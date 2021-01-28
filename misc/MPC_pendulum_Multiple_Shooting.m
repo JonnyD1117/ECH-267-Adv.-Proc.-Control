6bb9ae4f3d215097973a51f389fe6221094b61d3
jtdorsey22 <jonathandorsey1117@gmail.com>
2021-01-27 22:26:15 -0800

    Added multiple shooting version of inverted pendulum

--- misc/MPC_pendulum_Multiple_Shooting.m
+++ misc/MPC_pendulum_Multiple_Shooting.m
@@ -35,17 +35,6 @@ U = SX.sym('U', n_ctrl, N); % Inputs at each step K in horizon N
 P = SX.sym('P', 2*n_states);% Params at each step K in horizon N, P = (x1_0, x2_0, x1_ref, x2_ref)
 X = SX.sym('X', n_states, (N+1)); % States at each step K in horizon N
 
-% Initialize the State initial state vector using Parameter vector 
-X(:,1) = P(1:2); 
-
-% Using forward recursion, populate X matrix symbollically (Computational Graph)  
-for k = 1:N
-    st = X(:,k); con = U(:,k); % Define the current state and input    
-    f_value = f(st, con); % Use 'st' & 'con' to compute the state derivatives
-    st_new = st + (T*f_value); % Update Dynamics using Euler Integration
-    X(:,k+1) = st_new;    % Update state at next time step K+1
-end 
-
 % Using this symbolic representation of the State Update process, create a function to implement this behavior 
 ff = Function('ff',{U,P},{X}, {'Input','Reference '},{'New State'}); % This enables evaluation of symbolic graph give numerical inputs
 
@@ -56,25 +45,25 @@ g = [] ; % Initialize the Constraints as Empty
 % Define Loss function Weights on the States and Control Inputs
 Q = zeros(2,2); Q(1,1)=1; Q(2,2) =2 ; % Weighting matrices (states)
 R = .5; % Wieghting matrices (controls)
+%
+st = X(:,1); 
+g = [g; (st-P(1:2))]; % Initial Constraint via Defined Initial Condition
+
 
 % Compute Objective function from Stage Cost 
 for k=1:N
     % Symbolically Computes Graph for the objective function
-    st = X(:,k); con = U(:,k);
-    ref = P(3:4); 
-    obj = obj + (st - ref)'*Q*(st - ref) + con*R*con;   
+    st = X(:,k); con = U(:,k); ref = P(3:4); 
+    obj = obj + (st - ref)'*Q*(st - ref) + con*R*con; % Construct Symbolic representation of objective function at time step "K"   
+    st_next = X(:,(k+1));
+    f_value = f(st,con); 
+    st_next_euler = st + (T*f_value); 
+    g = [g; st_next-st_next_euler];
 end 
 
 
-% % Compute the Constraints Vector 
-% for k=1:N+1
-%     g = [g; X(1,k)]; % States X 
-%     g = [g; X(2,k)];
-% end 
-
-
 % Define Nonlinear Programming Structure 
-OPT_variables = U;  % Via Single Shooting only the Control Input set is used as the decision variable
+OPT_variables = [reshape(X, 2*(N+1), 1) ;U'];  % Via Single Shooting only the Control Input set is used as the decision variable
 nlp_prob = struct('f', obj, 'x', OPT_variables, 'g', g, 'p', P);
 opts = struct; 
 
@@ -91,24 +80,30 @@ solver = nlpsol('solver','ipopt', nlp_prob, opts);
 % Define Problem Arguments 
 args = struct;
 
-% Inequality Constraints
-args.lbg = 0; 
-args.ubg = 0; 
+% Inequality Constraints, for Entire Trajectory over Horizon
+args.lbg(1:2*(N+1)) = 0; 
+args.ubg(1:2*(N+1)) = 0; 
 
-%Input Constraints 
-args.lbx(1:1:N,1) = tau_min;
-args.ubx(1:1:N,1) = tau_max;
+%Combined State & Input Constraints (due to multishooting formulation)  
+args.lbx(1:2:2*(N+1),1) = -inf; % Theta Lower Bound
+args.ubx(1:2:2*(N+1),1) = inf;  % Theta Upper Bound
+args.lbx(2:2:2*(N+1),1) = -inf; % Theta_dot Lower Bound
+args.ubx(2:2:2*(N+1),1) = inf;  % Theta_dot Upper Bound
+
+args.lbx(2*(N+1):1: 2*(N+1) + N,1) = tau_min;   % Input Lower Bound 
+args.ubx(2*(N+1):1: 2*(N+1) + N,1) = tau_max;   % Input Upper Bound 
 
 
 % SIMULATION LOOP 
 t0 = 0; 
-% x0 = [(5*pi/4); 0] ;
-x0 = [0.5; 0] ;
+x0 = [(5*pi/4); 0] ;
+% x0 = [0.5; 0] ;
 x_ref = [pi;0] ;
 
 x_list(:,1) = x0; 
 t(1) = t0; 
 u0 = zeros(N,1);
+X0 = repmat(x0,1, N+1)';
 sim_time = 20; 
 
 % Start MPC 
@@ -123,12 +118,13 @@ main_loop = tic;
 while(norm((x0 - x_ref),2) > 1e-4 && mpc_iter < sim_time/T)
     
     args.p = [x0;x_ref];
-    args.x0 =  u0;
+    args.x0 =  [reshape(X0', 2*(N+1),1); reshape(u0',N,1)];
     sol = solver('x0', args.x0, 'lbx', args.lbx, 'ubx', args.ubx,'lbg', args.lbg, 'ubg', args.ubg, 'p', args.p); 
     
-    u = reshape(full(sol.x)',1,N)'; 
-    ff_value = ff(u, args.p); 
-    xxl(:, 1:2, mpc_iter +1) = full(ff_value)';
+    u = reshape(full(sol.x(2*(N+1)+1:end))',1,N)'; 
+    xxl(:, 1:2, mpc_iter +1) = reshape(full(sol.x(1:2*(N+1)))',2,N+1)';
+%     ff_value = ff(u, args.p); 
+%     xxl(:, 1:2, mpc_iter +1) = full(ff_value)';
     u_cl = [u_cl; u(1)]; 
     
     t(mpc_iter +1) = t0; 
